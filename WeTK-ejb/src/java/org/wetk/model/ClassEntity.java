ed7a005726c6462ec0fe1f50dbb067025113742d
Radek Ježdík <redhead@email.cz>
2012-04-12 17:18:23 +0200

    added unique constraint for ClassEntity.title

--- WeTK-ejb/src/java/org/wetk/model/ClassEntity.java
+++ WeTK-ejb/src/java/org/wetk/model/ClassEntity.java
@@ -27,7 +27,7 @@ public class ClassEntity extends AbstractEntity {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String title;
 
 	@OneToMany(mappedBy = "clazz")
