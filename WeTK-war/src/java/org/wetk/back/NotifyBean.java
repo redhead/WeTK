a6867c1c488ea49c6529d0dc5682e87c6ad5417f
Radek Ježdík <redhead@email.cz>
2012-05-13 21:33:12 +0200

    typos

--- WeTK-war/src/java/org/wetk/back/NotifyBean.java
+++ WeTK-war/src/java/org/wetk/back/NotifyBean.java
@@ -6,9 +6,6 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 import org.wetk.business.notificator.ClassTeacherNotificationLocal;
 
 
